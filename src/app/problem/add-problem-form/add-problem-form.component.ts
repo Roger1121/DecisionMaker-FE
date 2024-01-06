85740172c53b69295f0d50d4b37bdd3e6cf28aff
Roger1121 <leszczynski_szymon@wp.pl>
2024-01-06 23:28:22 +0100

    added criteria to problem form

--- src/app/problem/add-problem-form/add-problem-form.component.ts
+++ src/app/problem/add-problem-form/add-problem-form.component.ts
@@ -1,8 +1,8 @@
 import {Component, Output, EventEmitter} from '@angular/core';
 import {Problem} from "../../shared/model/problem";
-import {ReactiveFormsModule} from "@angular/forms";
+import {FormArray, ReactiveFormsModule} from "@angular/forms";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {NgIf} from "@angular/common";
+import {NgForOf, NgIf} from "@angular/common";
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {ProblemService} from "../problem.service";
 
@@ -11,7 +11,8 @@ import {ProblemService} from "../problem.service";
   standalone: true,
   imports: [
     ReactiveFormsModule,
-    NgIf
+    NgIf,
+    NgForOf
   ],
   templateUrl: './add-problem-form.component.html',
   styleUrl: './add-problem-form.component.css'
@@ -24,13 +25,43 @@ export class AddProblemFormComponent {
   problemForm = new FormGroup({
     name: new FormControl('', [Validators.required, Validators.maxLength(200)]),
     description: new FormControl('', [Validators.required, Validators.maxLength(2000)]),
-    group: new FormControl('', [Validators.required, Validators.maxLength(200)])
+    group: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+    criteria: new FormArray([
+      new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+        type: new FormControl('', [Validators.required, Validators.maxLength(200)])
+      })
+    ])
   });
+
+  addCriterion(){
+    const criteriaList = <FormArray>this.problemForm.controls['criteria'];
+    criteriaList.push(
+      new FormGroup({
+        name: new FormControl('', [Validators.required, Validators.maxLength(200)]),
+        type: new FormControl('', [Validators.required, Validators.maxLength(200)])
+      })
+    )
+  }
+
+  removeCriterion(index: number){
+    const criteriaList = <FormArray>this.problemForm.controls['criteria'];
+    criteriaList.removeAt(index);
+  }
 
   submitForm() {
     let problem = new Problem(this.problemForm.get('name')?.getRawValue(), this.problemForm.get('description')?.getRawValue(), false, this.problemForm.get('group')?.getRawValue());
-    this.problemService.addProblem(problem).subscribe((data) => {
-      this.activeModal.close('Success');
-    });
+    this.problemService.addProblem(problem).subscribe(
+      (data) => {
+        console.log(data);
+        this.activeModal.close('Success');
+      },
+      (error) => {
+        this.activeModal.close('Failure');
+      });
+  }
+
+  get criteria(){
+    return this.problemForm.get('criteria') as FormArray;
   }
 }
