d9bf83cee269e19c83c9a0843291e05b141d9268
Roger1121 <leszczynski_szymon@wp.pl>
2024-03-29 19:37:19 +0100

    survey filling

--- src/app/shared/model/question-response.ts
+++ src/app/shared/model/question-response.ts
@@ -1,7 +1,7 @@
 export class QuestionResponse{
-  constructor(
-    public question: number,
-    public content: string
-  ) {
+    constructor(
+        public question: number | undefined,
+        public content: string
+    ) {
   }
 }
--- src/app/survey/survey.component.ts
+++ src/app/survey/survey.component.ts
@@ -2,24 +2,28 @@ import {Component} from '@angular/core';
 import {SurveyService} from "./survey.service";
 import {EventService} from "../shared/services/EventService";
 import {Router} from "@angular/router";
-import {NgForOf, NgTemplateOutlet} from "@angular/common";
+import {NgForOf, NgIf, NgTemplateOutlet} from "@angular/common";
 import {Question} from "../shared/model/question";
 import {QuestionResponse} from "../shared/model/question-response";
 import {QuestionService} from "../question/question.service";
+import {Answer} from "../shared/model/answer";
+import {OptionComparison} from "../shared/model/option-comparison";
 
 @Component({
   selector: 'app-survey',
   standalone: true,
   imports: [
     NgForOf,
-    NgTemplateOutlet
+    NgTemplateOutlet,
+    NgIf
   ],
   templateUrl: './survey.component.html',
   styleUrl: './survey.component.css'
 })
 export class SurveyComponent {
   protected questions: Question[] = [];
-  private responses: QuestionResponse[] = [];
+  protected responses: QuestionResponse[] = [];
+  answers: Answer[] = [];
 
   constructor(private questionService: QuestionService,
               private surveyService: SurveyService,
@@ -46,11 +50,16 @@ export class SurveyComponent {
     }, (error) => {
       this.eventService.emit("alert-error", error.error);
     });
+    this.questionService.getAnswers().subscribe((data: any) => {
+      this.answers = data;
+    }, (error) => {
+      this.eventService.emit("alert-error", error.error);
+    });
     this.surveyService.getResponses().subscribe((data: any) => {
       this.responses = data;
     }, (error) => {
       this.eventService.emit("alert-error", error.error);
-    })
+    });
   }
 
   checkResponseExists(question_id: number | undefined) {
@@ -58,9 +67,61 @@ export class SurveyComponent {
   }
 
   getResponseContent(question_id: number | undefined) {
-    return this.responses.filter(resp => resp.question === question_id)[0].content;
+    let responses = this.responses.filter(resp => resp.question === question_id);
+    if(responses.length === 0){
+      return null;
+    } else {
+      return responses[0].content;
+    }
   }
-  saveResponses() {
 
+  getAnswersByQuestion(question_id: any) {
+    return this.answers.filter(answer => answer.question === question_id);
+  }
+
+  getQuestionResponse(question: Question) {
+    if (question.type === 'otwarte') {
+      let textarea: any = document.getElementById("response" + question.id?.toString(10));
+      if (textarea.value.trim().length === 0) {
+        return null;
+      } else {
+        return textarea.value.trim();
+      }
+    } else if (question.type === 'likert') {
+      for (let i = 1; i <= 5; i++) {
+        let radio: any = document.getElementById("" + question.id?.toString(10) + i.toString(10));
+        if (radio.checked) {
+          return radio.value.trim();
+        }
+      }
+    } else {
+      for (let i = 0; i < this.getAnswersByQuestion(question.id).length; i++) {
+        let radio: any = document.getElementById("" + question.id?.toString(10) + i.toString(10));
+        if (radio.checked) {
+          return radio.value.trim();
+        }
+      }
+    }
+    return null;
+  }
+
+  saveResponses() {
+    let responses: QuestionResponse[] = [];
+    this.questions.filter(q => this.getResponseContent(q.id) === null).forEach(question => {
+      let content = this.getQuestionResponse(question);
+      if (content !== null) {
+        responses.push(new QuestionResponse(question.id, content));
+      }
+    });
+    this.surveyService.saveResponses(responses).subscribe((data) => {
+      this.surveyService.getResponses().subscribe((data: any) => {
+        this.responses = data;
+      }, (error) => {
+        this.eventService.emit("alert-error", error.error);
+      });
+      this.eventService.emit("alert-success", data)
+    }, (error) => {
+      this.eventService.emit("alert-error", error.error);
+    });
   }
 }
